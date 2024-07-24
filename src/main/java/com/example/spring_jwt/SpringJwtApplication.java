180c189dd77211157a171c4123665bfdeea5e278
yousefnegmeldin <yousefnegm@gmail.com>
2024-07-24 12:02:58 +0300

    initial commit

---
+++ src/main/java/com/example/spring_jwt/SpringJwtApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtApplication.class, args);
+	}
+
+}
