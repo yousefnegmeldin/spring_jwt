7bd051bf0f6497cdb1416af9d81ab3048df2e15b
yousefnegmeldin <yousefnegm@gmail.com>
2024-08-12 10:45:33 +0300

    feat:added swagger

---
+++ src/main/java/com/example/spring_jwt/config/OpenAPIConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.spring_jwt.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+    @Bean
+    public OpenAPI defineOpenApi(){
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("dev");
+
+        Contact contact = new Contact();
+        contact.setEmail("yousefnegm@gmail.com");
+        contact.setName("yousef negm");
+
+        Info information = new Info()
+                .title("JWT APPLICATION")
+                .version("1.0")
+                .description("simple login and register api implementation, using jwt")
+                .contact(contact);
+        return new OpenAPI().info(information).servers(List.of(server));
+    }
+}
+
+
+
--- src/main/java/com/example/spring_jwt/config/SecurityConfiguration.java
+++ src/main/java/com/example/spring_jwt/config/SecurityConfiguration.java
@@ -21,6 +21,22 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2
+            "/auth/**",
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
+
     @Autowired
     public SecurityConfiguration(
             JwtAuthenticationFilter jwtAuthenticationFilter,
@@ -35,7 +51,7 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
